21309311a77f1d8705df4fbd2059a3fc237e4e25
sarkarbidhan07 <57905138+sarkarbidhan07@users.noreply.github.com>
2021-05-05 10:11:01 +0600

    Add files via upload

---
+++ f_to_c.py
@@ -0,0 +1,8 @@
+def main():
+    Fahrenheit=float(input("Enter the temp in Farenheight:"  ))
+    Celsius=(Fahrenheit-32)*5/9
+    print("temperature is ",Celsius)
+
+
+if __name__ == "__main__":
+    main()
